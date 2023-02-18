ed164369e2f57fef67e0243ca872b0382d7d9feb
Edvinas Venclavovic <edvinasvenclavovic@gmail.com>
2023-02-18 03:20:44 +0200

    Added submit form functionality

--- app/script.js
+++ app/script.js
@@ -1,1 +1,17 @@
-console.log("HELLO WORLD!");
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxOCPYG1NPSTZUwLn-N9f3C-M7ala2QTDYn6nqPGSSJCQeT5nJRcMUutDgtx-47oD28Zw/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Your message has been sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
